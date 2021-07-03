3c1d787eff4b3af7c2c30fe817901cbcad2a067c
tocchimittsu <arimagedon21@gmail.com>
2021-07-03 13:44:03 +0900

    修正

--- main.py
+++ main.py
@@ -17,21 +17,24 @@ class Application(tk.Frame):
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         frame.pack()
 
+        # 1. menubuttonを作成する
         # frame Widget(Frame)を親要素として、menubutton Widgetを作成する。
         # text : テキスト情報
         mb = tk.Menubutton(frame, text="Select alphabet")
 
-        # menubutton Widgetに紐づく、Menuを作成する。
+        # 2. menuを作成する
+        # menubutton Widgetを親要素として、menu Widgetを作成する。
         # Menuについて : https://kuroro.blog/python/ZITZ7dM4nundAhMbChXs/
         menu = tk.Menu(mb)
-        # Menu内に選択肢を追加する。
+        # menuの選択肢を追加する。
         # label : 選択肢のテキスト。'A'とする。
         menu.add_command(label="A")
-        # Menu内に選択肢を追加する。
+        # menuの選択肢を追加する。
         # label : 選択肢のテキスト。'B'とする。
         menu.add_command(label="B")
 
-        # menu optionへ作成したMenuを格納する。
+        # 3. menubutton内へmenuを入れる
+        # menubutton Widgetのmenu optionへ、作成したmenuを入れる。
         mb["menu"] = menu
 
         # frame Widget(Frame)を親要素とした場合に、menubutton Widgetをどのように配置するのか?
